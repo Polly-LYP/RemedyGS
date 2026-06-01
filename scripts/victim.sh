a367c49ce9fd68aca5f2908ae482bf0c93042edb
Polly-LYP <1315165648@qq.com>
2026-06-01 11:40:06 +0800

    init

--- scripts/victim.sh
+++ scripts/victim.sh
@@ -4,10 +4,10 @@ name_2="kitchen"
 
 (mkdir -p "./output/victim/$name_2/"
 python ./victim/gaussian-splatting/benchmark.py --gpu 6\
-    -s ./example/MIP_Nerf_360_eps16/$name_2/ -m ./output/victim/$name_2/ 
+    -s ./example/output/$name_2/ -m ./output/victim/$name_2/ 
 )&
 
 (mkdir -p "./output/victim/$name_1/"
 python ./victim/gaussian-splatting/benchmark.py --gpu 7\
-    -s ./example/MIP_Nerf_360_eps16/$name_1/ -m ./output/victim/$name_1/ 
+    -s ./example/output/$name_1/ -m ./output/victim/$name_1/ 
 )&
